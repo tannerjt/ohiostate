0cd1f90e9965e5f153e5cb6723b28a3d5e5d9b8d
Joshua Tanner <biolsurfer85@gmail.com>
2015-01-17 20:58:54 -0800

    Adding player cards

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var buckeyesApp = angular.module('buckeyesApp', []);
-
 buckeyesApp.controller('main', function ($scope, $http) {
+	$scope.features, $scope.geojson, $scope.featureGroup;
 	$scope.appTitle = "Ohio State Buckeyes - Where the Champions Come From";
-
 	// Initialize map
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	// and set heights dynamically
+	function getMapHeight() {
+		return window.innerHeight - $("footer").height() - $("nav").height();
+	}
+	function getCardHeight() {
+		return window.innerHeight - $("footer").height() - $("nav").height() - $("#options").height();
+	}
+	$("#mapContainer").height(getMapHeight());
+	$("#cards").height(getCardHeight());
+	var map = L.map('map');
 	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
@@ -12,10 +20,24 @@ buckeyesApp.controller('main', function ($scope, $http) {
 	// Get geojson
 	$http.get('./scripts/data/players.geojson')
 		.success(function(data, status, headers, config) {
-			L.geoJson(data).addTo(map);
+			$scope.features = data.features;
+			console.log(data);
+			$scope.geojson = L.geoJson(data);
+
+			$scope.featureGroup = L.featureGroup([$scope.geojson]);
+			$scope.featureGroup.addTo(map);
+
+			map.fitBounds($scope.featureGroup.getBounds());
 		})
 		.error(function(data, status, headers, config){
 
 		});
+
+	// Continuous dynamic resize
+	$(window).resize(function () {
+		$("#mapContainer").height(getMapHeight());
+		$("#cards").height(getCardHeight());
+		map.invalidateSize();
+	});
 
 });
